aded244f652513116153142a4cc2a53393adda11
mbashirli <mbashirlee@gmail.com>
2025-11-16 19:37:04 -0500

    add tokenizer

--- src/tokenization.hpp
+++ src/tokenization.hpp
@@ -20,15 +20,64 @@ public:
     }
 
     inline std::vector<Token> tokenize() {
+        std::vector<Token> tokens;
+        std::string buf;
+        while (peek().has_value()) {
+            if (std::isalpha(peek().value())) {
+                buf.push_back(consume());
+                while (peek().has_value() && std::isalnum(peek().value())) {
+                    buf.push_back(consume());
+                }
+
+                if (buf == "exit") {
+                    tokens.push_back({.type = TokenType::exit});
+                    buf.clear();
+                    continue;
+                }
+
+                std::cerr << "Invalid token!" << std::endl;
+                exit(EXIT_FAILURE);
+            }
+
+            if (std::isdigit(peek().value())) {
+                buf.push_back(consume());
+                while (peek().has_value() && std::isdigit(peek().value())) {
+                    buf.push_back(consume());
+                }
+                tokens.push_back({.type = TokenType::int_lit, .value = buf});
+                buf.clear();
+                continue;
+            }
+
+            if (peek().value() == ';') {
+                tokens.push_back({.type = TokenType::semi});
+                continue;
+            }
+
+            if (std::isspace(peek().value())) {
+                while (std::isspace(peek().value())) {
+                    consume();
+                }
+                continue;
+            }
+
+            std::cerr << "Invalid syntax!" << std::endl;
+            exit(EXIT_FAILURE);
+        }
+
+        return tokens;
     }
 
 private:
-
-    std::optional<char> peek() const {
+    [[nodiscard]] std::optional<char> peek() const {
         if (m_index + 1 >= m_src.length()) {
             return {};
         }
         return m_src.at(m_index);
+    }
+
+    char consume() {
+        return m_src.at(m_index++);
     }
 
     int m_index;
